d16da733f89d7ed793dea9dcbf0a48dca96e7954
Matthew Sadecki <14mds8@queensu.ca>
2016-12-01 22:40:13 -0600

    main

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+
+int main{
+	int a = 0;
+	while(i!=0){
+		printf("Type 1 to insert, 2 to remove, 3 to get data, or 4 to check length\n");
+		printf("Type 0 at anytime to quit");
+		scanf("%d",i);
+		if(i==1){
+			printf("Enter the index you wish to insert at: \n");
+			scanf("%d",a);
+			insert(a, Data data);
+		if(i==2){
+			printf("Enter index you wish to remove: \n");
+			scanf("%d",a);
+			remove(a);
+		if(i==3){
+			printf("Enter index you wish to get data for: \n");
+			scanf(%d,a);
+			get(a);
+		if(i==4){
+			length()
+		}
+	}
+}
