a0b4adc87862182755992f4b24299bf02aecc080
alexgadanidis <alex.gadanidis@gmail.com>
2016-12-01 23:53:13 -0500

    main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+//Josh Burak, Alexander Gadanidis, Kat Pinto, Matthew Sadecki
+//10169785 , 10186175, 10168919 , 10179995
+
+#include<stdlib.h>
+#include<stdio.h>
+#include "udll.h" 
+
+int main() {
+
+insert(1,5,1);
+insert(2,4,2);
+get(1);
+length();
+remove(1);
+get(1);
+length();
+
+} // end main
